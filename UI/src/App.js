92bf8c89e1c9be631fcdefe56cdf93ceaa4e365b
Laura Groetzinger <laura.groetzinger@gmail.com>
2018-03-19 22:31:57 -0400

    changed title, attempted to add semantic UI, got tired, tried to add bootstrap grid and I forget what else

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './splitter-logo.svg';
 import './App.css';
+import './grid.css';
 
 class App extends Component {
   render() {
@@ -9,18 +11,25 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <div className="cell">
-          <div className="caption">
-            5hr ago
-          </div>
-          <h1 className="h1">Playa Bowls</h1>
-          <div className="price">
-            $20.19
-          </div>
-        </div>
+        <Purchase time="5hrs ago" business="Playa Bowls" price="$20.62" />
       </div>
     );
   }
 }
+
+function Purchase(props) {
+  return (
+    <div className="Purchase">
+      <p>
+        <div className="caption">{props.time}</div>
+        <h1 className="h1">{props.business}</h1>
+        <div className="price">{props.price}</div>
+      </p>
+      <button></button>
+    </div>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 export default App;
