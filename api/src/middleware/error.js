ddc941e5d9b816dc759049dbf7295ca63cacc6aa
Lincoln de Sousa <lincoln@comum.org>
2018-03-24 13:49:28 -0400

    server: Remove MySQL specific error handling

--- api/src/middleware/error.js
+++ api/src/middleware/error.js
@@ -5,8 +5,8 @@ import { ValidationError } from '../lib/validation';
 /** Return the proper HTTP Error code based on the exception type */
 export function handleError(error: Error, req: $Request, res: $Response, next: Function) {
   if (error.stack) console.log(error.stack);
-  return res.sendStatus(error.code === 'ER_DUP_ENTRY' ? 409 : ({
+  return res.sendStatus({
     [AuthError.name]: 401,
     [ValidationError.name]: 400,
-  })[error.constructor.name] || 500);
+  }[error.constructor.name] || 500);
 }
