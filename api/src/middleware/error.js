6d252e810c8f93d96ff6c08a19129682cac403b4
Lincoln de Sousa <lincoln@comum.org>
2018-03-26 23:45:25 -0400

    server: Print out API errors to the console

--- api/src/middleware/error.js
+++ api/src/middleware/error.js
@@ -2,9 +2,34 @@
 import { AuthError } from '../lib/auth';
 import { ValidationError } from '../lib/validation';
 
-/** Return the proper HTTP Error code based on the exception type */
-export function handleError(error: Error, req: $Request, res: $Response, next: Function) {
+/** Property name that might contain an error message
+ *
+ * Different libraries use different attributes to store error
+ * messages. */
+const errorMessageProperties = [
+  'error_message',
+];
+
+/** Print out meaningful information within an error instance */
+function printError(error: Error) {
   if (error.stack) console.log(error.stack);
+  errorMessageProperties.every((property) => {
+    if (Object.hasOwnProperty.call(error, property)) {
+      console.log(error[property]);
+      return false;
+    }
+    return true;
+  });
+}
+
+/** Return the proper HTTP Error code based on the exception type */
+export function handleError(
+  error: Error,
+  req: $Request,
+  res: $Response,
+  next: Function) // eslint-disable-line no-unused-vars
+{
+  printError(error);
   return res.sendStatus({
     [AuthError.name]: 401,
     [ValidationError.name]: 400,
