665b314213a1be30c810c3250623e37a032c4cdc
Lincoln de Sousa <lincoln@comum.org>
2018-03-26 23:43:43 -0400

    server: Add function to create service & make libuser more generic

--- api/src/lib/stores.js
+++ api/src/lib/stores.js
@@ -7,7 +7,16 @@ import knex from '../models';
  */
 export async function createUser(options: any): Promise<Object> {
   const { name, email } = options;
-  await knex('user').insert({ name, email });
+  await knex('user').insert({ email });
   const [user] = await knex('user').where({ email });
   return user;
+}
+
+/** Create new a service account for a user */
+export async function createService(
+  user: UserType,
+  name: string,
+  data: Object): Promise
+{
+  await knex('service').insert({ userid: user.id, name, data });
 }
